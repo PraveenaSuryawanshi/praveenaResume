ce1acdb00436a3237069082b72ee37ba76a706bf
PraveenaSuryawanshi <praveena.ps7@gmail.com>
2022-06-24 22:21:40 +0530

    Updates

---
+++ precache-manifest.3bf5eb1b00b52ef245c704489d76c92d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cabf4fff6d945ea4fd25aa9929034b2",
+    "url": "/praveenaResume/index.html"
+  },
+  {
+    "revision": "51d1aeadfdc7c0c524ff",
+    "url": "/praveenaResume/static/css/main.d9eac4b3.chunk.css"
+  },
+  {
+    "revision": "16a2f32905b056c7503c",
+    "url": "/praveenaResume/static/js/2.3560a5d0.chunk.js"
+  },
+  {
+    "revision": "51d1aeadfdc7c0c524ff",
+    "url": "/praveenaResume/static/js/main.c6663d23.chunk.js"
+  },
+  {
+    "revision": "f33ba31b6ccc619ca653",
+    "url": "/praveenaResume/static/js/runtime-main.d712844e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/praveenaResume/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/praveenaResume/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/praveenaResume/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/praveenaResume/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/praveenaResume/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/praveenaResume/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/praveenaResume/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/praveenaResume/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4f9cf083efd015a030bc98dd0581ae95",
+    "url": "/praveenaResume/static/media/about.4f9cf083.gif"
+  },
+  {
+    "revision": "b5761f7d631b58330fe520b31f96c391",
+    "url": "/praveenaResume/static/media/advity.b5761f7d.webp"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/praveenaResume/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "b1f01ad0b076e623aaeb99a8b126dd88",
+    "url": "/praveenaResume/static/media/banner.b1f01ad0.gif"
+  },
+  {
+    "revision": "1b0047f70b1d43a03f3cfebbeeeae435",
+    "url": "/praveenaResume/static/media/banner2.1b0047f7.gif"
+  },
+  {
+    "revision": "2d3c486a2acb699bbe53c76b95288b5d",
+    "url": "/praveenaResume/static/media/contact.2d3c486a.gif"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/praveenaResume/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/praveenaResume/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/praveenaResume/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/praveenaResume/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/praveenaResume/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/praveenaResume/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/praveenaResume/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/praveenaResume/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/praveenaResume/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/praveenaResume/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/praveenaResume/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/praveenaResume/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/praveenaResume/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/praveenaResume/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/praveenaResume/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/praveenaResume/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "bb752afb73842d00bd61b20bfe8ae337",
+    "url": "/praveenaResume/static/media/logixstic.bb752afb.png"
+  },
+  {
+    "revision": "6692dc7cf54be502628f5b4eb6e6ea80",
+    "url": "/praveenaResume/static/media/looder.6692dc7c.gif"
+  },
+  {
+    "revision": "00de9022bc2312a25b53f03d2a4846b5",
+    "url": "/praveenaResume/static/media/mypic.00de9022.jpg"
+  },
+  {
+    "revision": "c8651c31340476d226f222001e255c1e",
+    "url": "/praveenaResume/static/media/projects.c8651c31.gif"
+  },
+  {
+    "revision": "c0c8218ed6a585844705a4411f4666d1",
+    "url": "/praveenaResume/static/media/testsigma-logo.c0c8218e.svg"
+  },
+  {
+    "revision": "d48164fb66e4954a40009de81e7491e3",
+    "url": "/praveenaResume/static/media/whaticando.d48164fb.gif"
+  }
+]);
